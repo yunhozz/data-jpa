a4d4a43d79618f0960ab20850bad48c61657110b
yunhozz <qkrdbsgh1121@naver.com>
2022-03-29 20:14:48 +0900

    first commit

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -9,11 +9,13 @@ import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    //select m from Member m where m.username = :username and m.age > :age
-    List<Member> findByUsernameAndAgeGreaterThan(String username, int age); //메소드 명으로 쿼리 생성, 이름이 길어지면 더러워진다.
+    //메소드 명으로 쿼리 생성, 이름이 길어지면 더러워진다.
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age); //select m from Member m where m.username = :username and m.age > :age
 
-    List<Member> findByUsername(@Param("username") String username); //using NamedQuery, 어플리케이션 동작 시점에 오류 검출, 거의 사용 x
+    //using NamedQuery, 어플리케이션 동작 시점에 오류 검출, 거의 사용 x
+    List<Member> findByUsername(@Param("username") String username);
 
+    //using Query annotation, 많이 사용
     @Query("select m from Member m where m.username = :username and m.age = :age")
-    List<Member> findUser(@Param("username") String username, @Param("age") int age); //using Query annotation, 많이 사용
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
