10350e6cc8eb682e555a81e954fd9bab2e86536b
yunhozz <qkrdbsgh1121@naver.com>
2022-03-29 11:18:46 +0900

    first commit

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,28 @@
+package study.datajpa.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String username;
+
+    public Member(String username) {
+        this.username = username;
+    }
+}
