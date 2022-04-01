5b96ae3f793d023208c67899778a9c62c162b619
yunhozz <qkrdbsgh1121@naver.com>
2022-04-01 17:25:11 +0900

    first commit

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -178,6 +179,7 @@ class MemberRepositoryTest {
 
         //when
         Page<Member> page = memberRepository.findByAge(age, pageRequest); //totalCount 쿼리도 생성 -> 성능 저하 우려 -> countQuery
+//        Slice<Member> slice = memberRepository.findByAge(age, pageRequest);
         Page<MemberDto> toMap = page.map(m -> new MemberDto(m.getId(), m.getUsername(), null)); //dto 로 변환
 
         //then
