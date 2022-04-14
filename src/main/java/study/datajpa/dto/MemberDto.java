a6ec09ed39ae37070075c3e0510d05df893a1fd8
yunhozz <qkrdbsgh1121@naver.com>
2022-04-14 11:06:09 +0900

    first commit

--- src/main/java/study/datajpa/dto/MemberDto.java
+++ src/main/java/study/datajpa/dto/MemberDto.java
@@ -10,12 +10,14 @@ public class MemberDto {
     private String username;
     private String teamName;
 
+    //request
     public MemberDto(Long id, String username, String teamName) {
         this.id = id;
         this.username = username;
         this.teamName = teamName;
     }
 
+    //response
     public MemberDto(Member member) {
         id = member.getId();
         username = member.getUsername();
